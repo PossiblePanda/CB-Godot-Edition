6416f3b931b63ef94ef2ada961cda4069d5fcff7
crtr127 <crtr127@proton.me>
2025-05-13 19:04:36 -0500

    Blink timer

--- scripts/components/blink_component.gd
+++ scripts/components/blink_component.gd
@@ -6,7 +6,6 @@ signal blinking_changed
 
 const BLINK_METER_MAX := 20
 const BLINK_METER_MIN := 0
-const BLINK_TIME := 0.1
 
 var can_see: bool:
 	get:
@@ -18,8 +17,8 @@ var blinking: bool = false:
 		blinking_changed.emit()
 		blinking = val
 var blink_meter = 20
-var tween : Tween
 
+@onready var blink_timer: Timer = $BlinkTimer
 @onready var blink_update: Timer = $"../BlinkUpdate"
 
 func _ready():
@@ -35,20 +34,33 @@ func blink_update_timeout():
 		full_blink()
 
 
+func await_blink_timer():
+	if not blink_timer.is_stopped():
+		await blink_timer.timeout
+	
+	blink_timer.start()
+	await blink_timer.timeout
+
+
 func show_blink():
+	if blinking == true:
+		return
 	blinking = true
 	
 	blink_meter = BLINK_METER_MIN
 	blink_update.start()
 	blink.emit()
 	
+	await_blink_timer()
 	#await Utils.tween_fade_in(blink_color, BLINK_TIME, 0, 0, "color:a").finished
 
 
 func hide_blink():
+	if blinking == false:
+		return
 	blinking = false
-	can_see = true
 	
+	await_blink_timer()
 	#await Utils.tween_fade_out(blink_color, BLINK_TIME, 0, 0, "color:a").finished
 	blink_meter = 20 # Reset bar to max
 
