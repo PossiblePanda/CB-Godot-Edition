2803f940e415dea8ec8c098a5b4bb3311f7ce762
crtr127 <crtr127@proton.me>
2025-05-19 15:35:13 -0500

    Fix saves

--- data/resources/base_save.gd
+++ data/resources/base_save.gd
@@ -3,7 +3,7 @@ class_name BaseSave
 extends Resource
 
 @export var save_version: int = 0
-@export var save_name: String = "save.tres"
+@export var save_name: String = "save"
 @export_storage var last_saved: float # unix timestamp
 
 func save_to_file(path: String):
